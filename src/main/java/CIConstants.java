ae5e1fdb1932731d0f3d7f51e72d6e3874adef78
hyodlee <darshu@naver.com>
2018-07-30 20:59:16 +0900

    Add files via upload

---
+++ src/main/java/CIConstants.java
@@ -0,0 +1,48 @@
+public class CIConstants {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String HEADER_PROCODE = "procode";
+    public static final String HEADER_ERRMSG = "errmsg";
+    public static final String INTERFACE_ID = "interfaceId";
+    public static final String SENDER_SYSTEM_ID = "sendersystemId";
+    public static final String SENDER_SYSTEM_PASSWD = "sendersystempasswd";
+    public static final String HTTP_METHOD = "httpmethod";
+    public static final String REST_URI = "resturi";
+    public static final String ACCEPT = "Accept";
+    public static final String ContentType = "Content-Type";
+
+    public static final String HEADER_SUCCES_CODE="0000";
+
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAIL = "FAIL";
+    public static final String RESULT_CODE = "RESULTCODE";
+    public static final String RESULT_MSG = "RESULTMSG";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String RESULT_CONTENT = "resultContent";
+
+    public static final String ERROR_CODE = "ERRORCODE";
+    public static final String ERROR_MSG = "ERRORMSG";
+
+
+    public static final String AUTH_RESULT_CODE_SUCCESS = "0000";
+    public static final String AUTH_RESULT_CODE_ER01 = "ER01";
+    public static final String AUTH_RESULT_CODE_ER02 = "ER02";
+
+    public static final String PROCESS_CODE_SUCCESS = "SUCC";
+    public static final String PROCESS_CODE_FAIL = "FAIL";
+
+    public static final String DATA_EXPORT_MESSAGE = "송수신 메시지";
+    public static final String DATA_EXPORT_UNFIRM = "비인가 접근 로그";
+    public static final String DATA_EXPORT_PASSWD = "패스워드 인증 로그";
+
+    public static final String DATA_EXPORT_MSG_FILE_PREFIX = "TRSMRCV_MESSAGE_";
+    public static final String DATA_EXPORT_UNFIRM_FILE_PREFIX = "UNCONFIRMED_ACCESS_LOG";
+    public static final String DATA_EXPORT_PASSWD_FILE_PREFIX = "PASSWORD_CRTFC_LOG";
+
+    public static final String STATUS_PROCESSED = "0000";
+    public static final String STATUS_PROCESSING = "9999";
+
+    public static final String STAT_COND_INTERFACE = "INTERFACE_ID";
+    public static final String STAT_COND_CATEGORY= "CATEGORY_ID";
+    public static final String STAT_COND_OFFER_SYS= "OFFER_SYS_ID";
+    public static final String STAT_COND_API_USER= "API_USER_ID";
+}
